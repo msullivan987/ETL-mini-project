e74683fbbdfd516ebe2a22523a5491a52ba4f28c
BilsonDS <69478154+BilsonDS@users.noreply.github.com>
2020-11-12 19:57:10 -0800

    netflix titles sql

---
+++ bechdel_netflix.sql
@@ -0,0 +1,20 @@
+CREATE TABLE release_year (
+show_id TEXT PRIMARY KEY,
+title TEXT
+);
+
+CREATE TABLE ratings (
+show_id TEXT PRIMARY KEY,
+title TEXT
+);
+
+CREATE TABLE netflix_movies (
+show_id TEXT PRIMARY KEY,
+title TEXT
+);
+
+CREATE TABLE netflix_titles (
+imdb_id TEXT PRIMARY KEY,
+title TEXT
+);
+	
