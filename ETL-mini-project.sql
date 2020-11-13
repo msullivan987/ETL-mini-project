bfdf80e96e9764d4af27927763ce0dab5377a466
BilsonDS <69478154+BilsonDS@users.noreply.github.com>
2020-11-12 18:02:30 -0800

    netflix_movies sql

---
+++ ETL-mini-project.sql
@@ -0,0 +1,14 @@
+CREATE TABLE netflix_movies (
+show_id TEXT PRIMARY KEY,
+title TEXT
+	);
+	
+CREATE TABLE ratings (
+show_id TEXT PRIMARY KEY,
+title TEXT
+);
+
+CREATE TABLE release_year (
+show_id TEXT PRIMARY KEY,
+title TEXT
+);
